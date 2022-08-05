65e55e92b00ab9feb93a4be4ec945438133d1614
BASKAR.K <110029191+Baskarkumar2313@users.noreply.github.com>
2022-08-05 17:56:13 +0530

    Add files via upload

---
+++ first2.java
@@ -0,0 +1,19 @@
+/*
+authour name:baskar.k
+program name:relational operator
+date :05/08/2022
+*/
+
+class first2
+{
+public static void main(String[]sname)
+{
+System.out.println(10<40);
+System.out.println(20<40);
+System.out.println(20<=20);
+System.out.println(2000>200);
+System.out.println(200>=200);
+System.out.println(true==false);
+System.out.println("raju"=="raju");
+}
+}
